5031e45525c1c844c110dc8b1c04ea2d89f81972
EMRE BARIŞ ERDEM <erdem.emre.baris@gmail.com>
2025-07-02 10:27:49 +0300

    Department Entity ve Dto eklendi.
    Signed-off-by: EMRE BARIŞ ERDEM <erdem.emre.baris@gmail.com>

---
+++ src/main/java/group2/intranet/project/domain/dtos/DepartmentDTO.java
@@ -0,0 +1,21 @@
+package group2.intranet.project.domain.dtos;
+
+import java.time.LocalDateTime;
+
+public class DepartmentDTO {
+
+    private Integer id;
+    private String name;
+    private String location;
+    private String email;
+    private LocalDateTime createdAt;
+
+    /*
+    private List<Integer> employeeIds;
+    private List<Integer> eventIds;
+    private List<Integer> announcementIds;
+    private List<Integer> newsIds;
+    private List<Integer> documentIds;
+    */
+
+}
